ec76c5e174787a57e86b466d120f028452c600d7
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-10 00:00:33 -0300

    Add post category and post

--- forum/topics/serializers.py
+++ forum/topics/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import Comment
+from .models import *
 
 # Create your serializers here.
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = '__all__'
+
+class PostCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostCategory
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
         fields = '__all__'
 
