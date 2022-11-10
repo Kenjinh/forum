7853b1d8204b0cc668289e78f2b16210a41778da
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-10 00:20:06 -0300

    Add ips urls

--- forum/topics/urls.py
+++ forum/topics/urls.py
@@ -3,4 +3,6 @@ from . import apis
 
 urlpatterns = [
     path("api/comment/", apis.CommentController.as_view(), name="Comment-API"),
+    path("api/post-category/", apis.PostCategoryController.as_view(), name="PostCategory-API"),
+    path("api/post/", apis.PostController.as_view(), name="Post-API"),
 ]
