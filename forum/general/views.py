5a3ff902eeb8f100c5d8cef48a954cd7b0f6a0ac
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-09 23:33:00 -0300

    Create atributes for Home Page

--- forum/general/views.py
+++ forum/general/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+
+class HomePageView(generics.ListAPIView):
+    template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        args = {
+            "title": "Home",
+            "permissions": "permissions",
+            "app_name": "Forum",
+        }
+        return render(request, template_name=self.template_name, context=args)
+
