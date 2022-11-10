afcbf032d0915013ccee8dfb062c8b606d11de73
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-09 23:40:34 -0300

    Create Posts Models

--- forum/topics/models.py
+++ forum/topics/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class PostCategory(models.Model):
+    category: str = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'tb_forum_topics_posts_ctg'
+
+
+class Post(models.Model):
+    category = models.ForeignKey(PostCategory, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateField(auto_created=True)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        db_table = 'tb_forum_topics_posts'
+
+
+class Comment(models.Model):
+    post: int = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateField(auto_created=True)
+    comment: str = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = 'tb_forum_topics_comments'
+
