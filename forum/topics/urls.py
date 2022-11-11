4cd37a7491c106ea15076170bb037fa3d5e5bb37
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-10 22:18:42 -0300

    Add PostDetailView on path

--- forum/topics/urls.py
+++ forum/topics/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import apis
+from . import views
 
 urlpatterns = [
     path("api/comment/", apis.CommentController.as_view(), name="Comment-API"),
     path("api/post-category/", apis.PostCategoryController.as_view(), name="PostCategory-API"),
     path("api/post/", apis.PostController.as_view(), name="Post-API"),
+    re_path("(?P<pk>\d+)/", views.PostDetailView.as_view(), name="PostDetail")
 ]
