cdf559d3a0444b1e4e109a15875d57247f8b1ef5
gnohara <gnohara@idc.com>
2022-11-18 17:08:50 -0300

    Remove request

--- forum/general/views.py
+++ forum/general/views.py
@@ -8,7 +8,6 @@ from topics.models import *
 
 class HomePageView(generics.ListAPIView):
     template_name = "home.html"
-    categories = PostCategory.objects.all()
     form = NewPostForm()
 
     def get(self, request, *args, **kwargs):
@@ -16,7 +15,6 @@ class HomePageView(generics.ListAPIView):
             "title": "Home",
             "permissions": "permissions",
             "app_name": "Forum",
-            "categories": self.categories,
             "form": self.form
         }
         return render(request, template_name=self.template_name, context=args)
